ef51bea48d337086a4be176b24961c135926db7b
bruce <brucedrummond@gmail.com>
2015-05-27 15:24:14 -0400

    poscene resources not required

--- samples/SpritesheetAnimation/include/Resources.h
+++ samples/SpritesheetAnimation/include/Resources.h
@@ -1,7 +1,3 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-#define RES_GLSL_PO_SHAPE_VERT	CINDER_RESOURCE( resources/poSceneResources/shaders/, poShape_vert.glsl, 133, GLSL )
-#define RES_GLSL_PO_SHAPE_FRAG	CINDER_RESOURCE( resources/poSceneResources/shaders/, poShape_frag.glsl, 134, GLSL )
-#define RES_GLSL_PO_MASK_VERT	CINDER_RESOURCE( resources/poSceneResources/shaders/, poMask_vert.glsl, 135, GLSL )
-#define RES_GLSL_PO_MASK_FRAG	CINDER_RESOURCE( resources/poSceneResources/shaders/, poMask_frag.glsl, 136, GLSL )
